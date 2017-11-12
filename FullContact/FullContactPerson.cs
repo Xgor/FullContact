f45823ee0adbf4dbabf6b632e3d87aca03af174d
Xgor <real.xgor@hotmail.com>
2017-11-12 14:02:15 +0100

    Class created for Data

--- FullContact/FullContactPerson.cs
+++ FullContact/FullContactPerson.cs
@@ -14,19 +14,32 @@ namespace FullContact
 {
     public interface IFullContactApi
     {
-       Task<FullContactPerson> 
+        Task<FullContactPerson> 
             LookupPersonByEmailAsync(string email);
     }
 
+    // FullContactPerson is containing
+    public class FullContactPerson
+    {
+        public string familyName, givenName, fullName;
+        public List<string> middleNames;
 
-    public class FullContactPerson : IFullContactApi
+        public int age;
+        public string city, continent, country, county, deducedLocation, gender, NormalizedLocation, State;
+        public decimal likelihood,locationLikelihood;
+
+       
+        public string Organizations;
+    }
+
+    public class FullContactSearch : IFullContactApi
     {
         
         public async Task<FullContactPerson> LookupPersonByEmailAsync(string email)
         {
 
-            var container = new Container();
-
+            Container container = new Container();
+            FullContactPerson p = new FullContactPerson();
             try
             {
                 #region Dependency Injection (DI) using Simple Injector
@@ -36,6 +49,7 @@ namespace FullContact
                 container.Register<IHttpClientFactory, HttpClientFactory>();
 
                 #endregion
+                
                 ///Get container full contact app service factory  
                 var fullContactAppServiceFactory = container.GetInstance<IFullContactAppServiceFactory>();
 
@@ -44,23 +58,56 @@ namespace FullContact
 
                 /// Call full contact api by get 
                 var person = await fullContactAppService.GetAsync(Lookup.Email, email);
+                //        person.Status
+                
 
-                System.Console.Write(fullContactAppService.ResponseSerializer.Serialize(person).Result);
+                p.familyName = person.ContactInfo.FamilyName;
+                p.givenName = person.ContactInfo.GivenName;
+                p.fullName = person.ContactInfo.FullName;
+                p.middleNames = person.ContactInfo.MiddleNames;
 
-                Console.WriteLine("Mail Loaded!");
+                p.likelihood = person.Likelihood;
+                
+                p.age = person.Demographics.locationDeduced.Age;
+                if(person.Demographics.locationDeduced.City != null)
+                    p.city = person.Demographics.locationDeduced.City.Name;
+                
+                p.continent = person.Demographics.locationDeduced.Continent.Name;
+                if(person.Demographics.locationDeduced.Country != null)
+                    p.country = person.Demographics.locationDeduced.Country.Name;
+
+                if (person.Demographics.locationDeduced.county != null)
+                    p.county = person.Demographics.locationDeduced.county.Name;
+
+                p.deducedLocation = person.Demographics.locationDeduced.DeducedLocation;
+
+                p.gender= person.Demographics.locationDeduced.Gender.ToString();
+                p.locationLikelihood=person.Demographics.locationDeduced.Likelihood;
+
+                if (person.Demographics.locationDeduced.State != null)
+                    p.State= person.Demographics.locationDeduced.State.Name;
+                
+                p.NormalizedLocation=person.Demographics.locationDeduced.NormalizedLocation;
+                
+                
+        //        System.Console.Write(fullContactAppService.ResponseSerializer.Serialize(person).Result);
+
+                return p;
             }
             catch (Exception ex)
             {
                 System.Console.WriteLine("Unexpected error: " + ex.Message);
-                
+
             }
 
-         //   container.RegisterSingleton<IServiceProvider>(container);
-        //    FullContactPerson person = new FullContactPerson();
-        //    await Task.Delay(5000);
-            
+            //   container.RegisterSingleton<IServiceProvider>(container);
+            //    FullContactPerson person = new FullContactPerson();
+            //    await Task.Delay(5000);
+
             return null;
         }
-
     }
+
+
+
 }
--- FullContact/Program.cs
+++ FullContact/Program.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Timers;
+
 namespace FullContact
 {
     class Program
     {
-
+        
         static void Main(string[] args)
         {
             Task t = MainAsync(args);
@@ -17,21 +19,36 @@ namespace FullContact
 
         static async Task MainAsync(string[] args)
         {
-            
 
             Console.Write("Enter E-mail: ");
             
             string email = Console.ReadLine();
-            FullContactPerson person = new FullContactPerson();
+            FullContactSearch search = new FullContactSearch();
 
-            Task<FullContactPerson> task = person.LookupPersonByEmailAsync(email);
+            Task<FullContactPerson> task = search.LookupPersonByEmailAsync(email);
+            Console.Write("Loading email");
 
-            Console.WriteLine("Loading email... ");
-                
+            Timer timer = new Timer(100);
+            timer.Elapsed += async (sender, e) => await AddLoadingDot();
+            timer.Start();
+
+
             FullContactPerson p = await task;
-            
+            timer.Stop();
+            Console.WriteLine("\nMail Loaded!");
+
+
+            Console.WriteLine("Name: " + p.fullName);
+            Console.WriteLine("Gender: "+p.gender);
+
             Console.ReadLine();
         }
+        
 
+        static Task AddLoadingDot()
+        {
+            Console.Write(".");
+            return Task.FromResult(0);
+        }
     }
 }
